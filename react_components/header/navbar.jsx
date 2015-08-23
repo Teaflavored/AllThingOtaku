4bdf5d551ba9edb48fad24fa55457755b087e071
Auster Chen <auster.s.chen@gmail.com>
2015-08-22 17:37:38 -0700

    added bootstrap and also started working on the navbar

--- react_components/header/navbar.jsx
+++ react_components/header/navbar.jsx
@@ -1,15 +1,24 @@
 var React = require("react");
+var navbarCSS = require("./navbar.js.css");
+// need to move the input into its own component, needs to handle auto complete and searching, can do later
 var Navbar = React.createClass({
-	test : function () {
-				console.log("finally works");
-		   },
 	render: function () {
 				return (
-						<div id="navbar" onClick={this.test}>
-							"Test worked!"
+						<div id="navbar" className="navbar navbar-fixed-top" style={navbarCSS.headerContainer}>
+							<div className="container">
+								<div className="navbar-header">
+									<h1 style={navbarCSS.brandHeader}><a className="navbar-brand" href="/">AllThingsOtaku</a></h1>
+								</div>
+								<input type="text" className="form-control input-lg" placeholder="Search for your favorite Anime, Manga, or Light Novels"></input>
+								<ul className="">
+									<li></li>
+									<li></li>
+									<li></li>
+								</ul>
+							</div>
 						</div>
 					);
-			}	
+	}			
 });
 
 module.exports = Navbar;
