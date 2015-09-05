2e30b00996431ffa4aa8e3ae53daf067c1e2677c
Auster Chen <auster.s.chen@gmail.com>
2015-09-04 21:35:41 -0700

    db config change

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 var DB_URL = process.env.DB_URL;
 var DB_NAME = process.env.DB_NAME;
-
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 var gracefulExit = function () {
     mongoose.connection.close(function () {
         console.log("Mongoose connection to " + DB_NAME + " has been closed.");
@@ -24,7 +25,7 @@ var connect = function (serverStartCB) {
     });
 
     try {
-        mongoose.connect(process.env.MONGOLAB_URI);
+        mongoose.connect(process.env.MONGOLAB_URI, options);
     } catch (err) {
         console.log("Failed to initialize server", err.message);
     }
